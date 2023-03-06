80e72661ecece72bee5e64af83e1b9e3791b2247
Natalie Hummel <69334473+nmhummel@users.noreply.github.com>
2023-03-05 18:20:57 -0600

    added url to local storage; got gallery setup

---
+++ gallery.js
@@ -0,0 +1,24 @@
+let retrievedPaintings = window.localStorage; //retrieve the object4
+let myGallery = document.getElementById("myGallery");
+let parsedStorage = [];
+let keys = Object.keys(retrievedPaintings)
+
+console.log(retrievedPaintings)
+console.log("keys", keys)
+
+keys.forEach(key => {
+    // debugger
+    let item = localStorage.getItem(key)
+    parsedStorage.push(JSON.parse(item))
+})
+
+console.log("parsedStorage", parsedStorage)
+
+parsedStorage.forEach(painting => {
+    debugger
+    let artUrl = painting.paintingUrl;
+    let artImg = document.createElement("img")
+    artImg.setAttribute("src", artUrl);
+    myGallery.appendChild(artImg);
+    let howdy;
+})
--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+let gallery = [];
+
+// search submit event
 input__form.addEventListener("submit", (e) => {
     e.preventDefault();
-    storeData();
+    console.log("e", e)
+    retrieveArt();
 });
 
-function storeData() {
+function retrieveArt() {
     const searchTerm = user__input.value.toLowerCase();
     makeCard(searchTerm);
     input__form.reset();
@@ -28,11 +32,14 @@ function displayImgs(searchTerm) {
       .then((response) => response.json())
       .then((images) => {
         const results = images.data;
+        console.log("RESULTS", results)
         results.map((result) => {
           const jpg__url = `https://www.artic.edu/iiif/2/${result.image_id}/full/843,/0/default.jpg`;
-  
+          let paintingLink = 'paintingUrl';
+          result[paintingLink] = jpg__url;
+        //   console.log("result", result)
           // create a new card div for each image
-          const cardDiv = createCardDiv();
+          const cardDiv = createCardDiv(result);
           art__here.appendChild(cardDiv);
   
           // add the image element to the card div
@@ -40,38 +47,57 @@ function displayImgs(searchTerm) {
           cardDiv.appendChild(img);
   
           // add the button elements to the card div
-          const btnDiv = createBtnDiv();
+          const btnDiv = createBtnDiv(result);
           cardDiv.appendChild(btnDiv);
-          
         });
-      });
-  }
+    });
+}
   
-  function createCardDiv() {
+function createCardDiv(painting) {
     const cardDiv = document.createElement("div");
     cardDiv.setAttribute("class", "cardDiv");
+    cardDiv.setAttribute("id", painting.id)
     return cardDiv;
-  }
+}
   
-  function createImg(jpg__url, title) {
+function createImg(jpg__url, title) {
     const img = document.createElement("img");
     img.setAttribute("class", "card-img-top");
     img.setAttribute("src", jpg__url);
     img.setAttribute("alt", title);
     img.style.width = "15rem";
     img.style.height = "15rem";
+    // console.log("IMG", img) 
+    // <img class="card-img-top" src="https://www.artic.edu/iiif/2/872371ff-d773-f209-8062-7f88c95f2691/full/843,/0/default.jpg" alt="Portrait of Sylvette David" style="width: 15rem; height: 15rem;">
     return img;
-  }
+}
   
-  function createBtnDiv() {
+function createBtnDiv(result) {
+    // console.log("result", result)
     const btnDiv = document.createElement("div");
-    btnDiv.setAttribute("id", "card-btns");
+    btnDiv.setAttribute("class", "saveButton");
     const saveBtn = document.createElement("button");
     saveBtn.setAttribute("type", "button");
     saveBtn.setAttribute("class", "btn");
     saveBtn.setAttribute("class", "btn-primary");
     btnDiv.appendChild(saveBtn);
     saveBtn.textContent = "Save";
+    saveBtn.setAttribute("id", result.id)
+    saveBtn.addEventListener('click', (e) => savePainting(e, result));
     return btnDiv;
-  }
+}
+
+
+function savePainting(e, result) {
+    console.log("ID",e, result)
+    // var paintingId = document.getElementById(result.id)
+    localStorage.setItem(result.id, JSON.stringify(result))
+    gallery.push(result)
+    console.log("gallery", gallery)
+}
+
   
+// localStorage.setItem('painting', JSON.stringify(painting)); //stringify object and store
+// var retrievedPainting = JSON.parse(localStorage.getItem('painting')); //retrieve the object
+// gallery.push(painting2)
+// localStorage.setItem('gallery', JSON.stringify(gallery)); //stringify object and store
